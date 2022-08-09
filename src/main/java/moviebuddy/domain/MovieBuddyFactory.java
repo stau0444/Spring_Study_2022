d64f4b78befb97ffb820a53f7b4d1f35b2697417
stau04@gmail.com <stau04@gmail.com>
2022-08-09 23:12:36 +0900

    bean scope

--- src/main/java/moviebuddy/domain/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/domain/MovieBuddyFactory.java
@@ -1,13 +1,46 @@
 package moviebuddy.domain;
 
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
+@Import({
+        MovieBuddyFactory.DataSourceModuleConfig.class,
+        MovieBuddyFactory.DomainModuleConfig.class
+})
 public class MovieBuddyFactory {
+
+    /*
+    메서드 콜 방식의 DI
     @Bean
     public MovieFinder movieFinder(){
-        return new MovieFinder(new CsvMovieReader());
+        return new MovieFinder(movieReader());
+    }
+    */
+
+    /*
+    메서드 파라미터 방식의 DI
+    - 스프링이 스스로 MovieReader가 bean으로 등록되어있는지 확인하고 주입해준다.
+     */
+
+    @Configuration
+    static class DataSourceModuleConfig{
+        @Bean
+        public MovieReader movieReader(){
+            return new CsvMovieReader();
+        }
+    }
+    @Configuration
+    static class DomainModuleConfig{
+        @Bean
+        @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+        public MovieFinder movieFinder(MovieReader movieReader){
+            return new MovieFinder(movieReader);
+        }
+
     }
 }
---
+++ src/test/java/moviebuddy/domain/BeanScopeTest.java
@@ -0,0 +1,19 @@
+package moviebuddy.domain;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanScopeTest {
+
+    @Test
+    void Equals_MovieFinderBean(){
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
+        MovieFinder movieFinder = ctx.getBean(MovieFinder.class);
+        MovieFinder movieFinder2 = ctx.getBean(MovieFinder.class);
+
+        Assertions.assertEquals(movieFinder,movieFinder2);
+    }
+}
